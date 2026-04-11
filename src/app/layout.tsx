4fd6643013e54394ba57a4a923b5821c3dc92ac5
asmitsingh000 <asmitsingh00091@gmail.com>
2026-04-11 22:57:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,17 @@ import "../styles/index.css";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Portfolio of Glaze",
-  description: "Welcome to our portfolio. visit it and see how it is befor any decision",
+  title: "Glaze_digital_Agency",
+  description: "Welcome to our website. Visit it and see how it is befor any decision",
   openGraph: {
-    title: "Portfolio of Glaze",
-    description: "Welcome to our portfolio. visit it and see how it is befor any decision",
+     title: "Glaze_digital_Agency",
+  description: "Welcome to our website. Visit it and see how it is befor any decision",
     images: ["/photos/main/portfolio.png"],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Portfolio of Glaze",
-    description: "Welcome to our portfolio. visit it and see how it is befor any decision",
+    title: "Glaze_digital_Agency",
+  description: "Welcome to our website. Visit it and see how it is befor any decision",
     images: ["/photos/main/portfolio.png"],
   },
   metadataBase: new URL("https://glazedizitalagency.vercel.app//"),
